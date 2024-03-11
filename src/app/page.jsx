7eb29cb707e89d821bdeb59aa37d4e1ba04af83f
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-03-11 15:46:57 -0300

    add jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className='flex items-center justify-center w-screen h-screen bg-violet-600'>
+      <h1 className='text-yellow-600 text-9xl'>SAVE</h1>
+    </main>
+  );
+}
